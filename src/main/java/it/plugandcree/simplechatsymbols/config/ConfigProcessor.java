a460b7c8aa04b138d7b71b58532efacd60a003e1
PlugAndCree <nicolamarangon123@gmail.com>
2021-04-02 12:27:56 +0200

    Added configurable regex

--- src/main/java/it/plugandcree/simplechatsymbols/config/ConfigProcessor.java
+++ src/main/java/it/plugandcree/simplechatsymbols/config/ConfigProcessor.java
@@ -6,13 +6,17 @@ import java.util.Map;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import it.plugandcree.simplechatsymbols.SimpleChatSymbols;
+
 public class ConfigProcessor {
 	public static Map<String, String> getSymbols(YamlConfiguration config, String configLine) {
 		Map<String, String> symbols = new HashMap<>();
 		ConfigurationSection configSection = config.getConfigurationSection(configLine);
+		CustomConfig lang = SimpleChatSymbols.getInstance().getLangConfig();
+		
 		
 		for(String key : configSection.getKeys(false)) {
-			symbols.put(String.format("[%s]", key), configSection.getString(key));
+			symbols.put(String.format(lang.getRawString("messages.symbol-format"), key), configSection.getString(key));
 		}
 		
 		return symbols;
